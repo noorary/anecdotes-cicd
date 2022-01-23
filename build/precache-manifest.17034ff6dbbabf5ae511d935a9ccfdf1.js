3740ba6a5f44ac5f3dd08e8d7b9650ac0d606ed6
noorarytila <rytilanoora@gmail.com>
2022-01-23 18:43:55 +0200

    dont ignore build

---
+++ build/precache-manifest.17034ff6dbbabf5ae511d935a9ccfdf1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9df2512a2aab7adc5ac1ed8bdf514fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5275bef269e94d3ddf4b",
+    "url": "/static/js/2.1a1ae180.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.1a1ae180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f052f5ffadbb70054cab",
+    "url": "/static/js/main.4f00530a.chunk.js"
+  },
+  {
+    "revision": "7938b11ffe0edea53eac",
+    "url": "/static/js/runtime-main.2b99d147.js"
+  }
+]);
